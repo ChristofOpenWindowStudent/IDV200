e7fdca2f08d5edbbe615b1bd99420c19b4ce0495
Christof Enslin <christof+something@openwindow.co.za>
2020-10-20 05:06:19 +0200

    switched to mysql

--- migrations/Version20201020025557.php
+++ migrations/Version20201020025557.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201006072131 extends AbstractMigration
+final class Version20201020025557 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,12 +20,16 @@ final class Version20201006072131 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE user_profile (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user_profile (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL)');
+        $this->addSql('CREATE TABLE friendship (sender_id INTEGER NOT NULL, receiver_id INTEGER NOT NULL, PRIMARY KEY(sender_id, receiver_id))');
+        $this->addSql('CREATE INDEX IDX_7234A45FF624B39D ON friendship (sender_id)');
+        $this->addSql('CREATE INDEX IDX_7234A45FCD53EDB6 ON friendship (receiver_id)');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE user_profile');
+        $this->addSql('DROP TABLE friendship');
     }
 }
